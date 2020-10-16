36d4e204177c213d682ae1b98d082b8cdd7db573
Erick Gonzalez <ericksinbox@gmail.com>
2020-10-16 13:20:34 -0400

    added option to select choice of sauce on pizza form

--- src/PizzaForm.js
+++ src/PizzaForm.js
@@ -7,7 +7,43 @@ export default function PizzaForm() {
     return (
         <>
         <div>
-            <p>Build Your Own Pizza!</p>
+            <h2>Build Your Own Pizza</h2>
+
+            <form>
+            <p class="form-title-section">Select a size</p>
+            <label class="sizes">
+                <select type="select" name="pizza-size">
+                    <option value="">--Please select a size--</option>
+                    <option value="small">Small</option>
+                    <option value="medium">Medium</option>
+                    <option value="large">Large</option>
+                    <option value="rediculous">Simma down na!</option>
+                </select>
+            </label>
+            <br />
+            <div class="sauces">
+                <p class="form-title-section">Choice of Sauce</p>
+                <label>Red
+                    <input type="radio" name="sauce" value="red" 
+                    />
+                </label><br />
+
+                <label class="sauces">Garlic Ranch
+                    <input type="radio" name="sauce" value="garlicranch" 
+                    />
+                </label><br />
+                <label>BBQ
+                    <input type="radio" name="sauce" value="bbq" 
+                    />
+                </label><br />
+                <label>Spinach Alfredo
+                    <input type="radio" name="sauce" value="spinachalfredo" 
+                    />
+                </label>
+
+            </div>
+            
+            </form>
         </div>
 
 
